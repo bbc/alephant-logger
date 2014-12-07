54ecef7304e1c63f2f354b5d5afb97b0d2873f83
Integralist <mark.mcdx@gmail.com>
2014-12-07 13:36:09 +0000

    Remove increment code (CloudWatch graphs display correctly when using 'sum' option)

--- lib/alephant/logger/cloudwatch_decorator.rb
+++ lib/alephant/logger/cloudwatch_decorator.rb
@@ -14,17 +14,17 @@ module Alephant
       def metric(opts)
         name, value, unit, dimensions = opts.values_at(:name, :value, :unit, :dimensions)
 
-        Thread.new {
+        Thread.new do
           cloudwatch.put_metric_data(
             :namespace => namespace,
             :metric_data => [{
               :metric_name => name,
-              :value       => determine(name, value),
+              :value       => value,
               :unit        => unit || "None",
               :dimensions  => parse(dimensions || {})
             }]
           )
-        }
+        end.join
       end
 
       # Ruby's Kernel implements a `warn` method
@@ -51,41 +51,6 @@ module Alephant
             :value => value
           }
         end
-      end
-
-      def determine(name, value)
-        value ? value : increment_metric_value(name)
-      end
-
-      def increment_metric_value(name)
-        stats = get_stats name
-        stats.datapoints.size == 0 ? 0 : stats.datapoints.last[:sum] += 1
-      end
-
-      def get_stats(name)
-        stats_for(filter_metric name).tap do |stats|
-          sort stats.datapoints if stats.datapoints.size > 0
-        end
-      end
-
-      def filter_metric(name)
-        filter_namespace.filter("metric_name", name)
-      end
-
-      def filter_namespace
-        cloudwatch.metrics.filter("namespace", namespace)
-      end
-
-      def stats_for(metric)
-        metric.first.statistics({
-          :statistics => ["Sum"],
-          :start_time => Time.now - ONE_HOUR,
-          :end_time => Time.now
-        })
-      end
-
-      def sort(stats)
-        stats.sort! { |a, b| a[:timestamp] <=> b[:timestamp] }
       end
     end
   end
