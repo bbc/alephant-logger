3f153fc31d5fcecde5d70d21c4153f1800d0e1d0
Dan Arnould <dan@arnould.co.uk>
2015-01-26 17:52:52 +0000

    Add .create for Logger

--- lib/alephant/logger.rb
+++ lib/alephant/logger.rb
@@ -2,26 +2,32 @@ require 'alephant/logger/version'
 require 'logger'
 
 module Alephant
-  class Logger
-    def initialize(drivers = [])
-      @drivers = drivers << ::Logger.new(STDOUT)
+  module Logger
+    def self.create(drivers = [])
+      Logger.new drivers
     end
 
-    def method_missing(name, *args)
-      drivers.each do |driver|
-        driver.send(name, *args) if driver.respond_to? name
+    class Logger
+      def initialize(drivers)
+        @drivers = drivers << ::Logger.new(STDOUT)
       end
-    end
 
-    def respond_to?(name)
-      drivers.any? do |driver|
-        driver.respond_to?(name) || super
+      def method_missing(name, *args)
+        drivers.each do |driver|
+          driver.send(name, *args) if driver.respond_to? name
+        end
       end
+
+      def respond_to?(name)
+        drivers.any? do |driver|
+          driver.respond_to?(name) || super
+        end
+      end
+
+      private
+
+      attr_reader :drivers
     end
-
-    private
-
-    attr_reader :driver
   end
 end
 
