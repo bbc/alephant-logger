01db9ef9e76324538d6a089305a20d68fcc7a7df
Steven Jack <stevenmajack@gmail.com>
2015-02-05 16:18:45 +0000

    Remove optional []

--- lib/alephant/logger_factory.rb
+++ lib/alephant/logger_factory.rb
@@ -2,7 +2,7 @@ require "alephant/logger/base"
 
 module Alephant
   module LoggerFactory
-    def self.create(drivers = [])
+    def self.create(drivers)
       Alephant::Logger::Base.new drivers
     end
   end
